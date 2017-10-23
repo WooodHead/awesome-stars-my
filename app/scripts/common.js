41e019d91e139b7beb246c46891aa0701d5d97a5
WooodHead <WooodHead@users.noreply.github.com>
2017-10-23 18:10:52 +0800

    add

---
+++ app/scripts/common.js
@@ -0,0 +1,11 @@
+import format from 'date-fns/format';
+
+exports.ERROR = '@@ERROR';
+
+exports.log = function log(...args) {
+  if (process.env.NODE_ENV === 'development') {
+    const timestamp = format(new Date(), 'YYYY-MM-DDTHH:mm:ssZ');
+    // eslint-disable-next-line no-console
+    console.log.apply(null, [`[${timestamp}]`, ...args]);
+  }
+};
